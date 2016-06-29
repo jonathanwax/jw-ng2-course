e80edcaf1c63ede0768fe67f5ceca7368d70c765
Jonathan Wax (Prodware) <jwax@prodware.fr>
2016-06-29 09:20:49 +0300

    deleted

--- A-Day3-Angular2-Components/inventory-app/dist/app/price-display/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./price-display.component'));
-//# sourceMappingURL=index.js.map
--- A-Day3-Angular2-Components/inventory-app/dist/app/shared/product.model.spec.js
+++
@@ -1,9 +0,0 @@
-/* tslint:disable:no-unused-variable */
-"use strict";
-var testing_1 = require('@angular/core/testing');
-testing_1.describe('Product', function () {
-    // it('should create an instance', () => {
-    //   expect(new Product()).toBeTruthy();
-    // });
-});
-//# sourceMappingURL=product.model.spec.js.map
